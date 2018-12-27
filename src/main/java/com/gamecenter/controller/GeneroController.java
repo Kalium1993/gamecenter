7c588cf986279538cd70b6b10f10ee3cf881dec0
Tecnico - TI <tecnico@epfserver.com.br>
2018-12-27 21:52:10 -0200

    fazendo controladores 2

--- src/main/java/com/gamecenter/controller/GeneroController.java
+++ src/main/java/com/gamecenter/controller/GeneroController.java
@@ -11,10 +11,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gamecenter.domain.Genero;
 import com.gamecenter.dto.GeneroDTO;
 import com.gamecenter.service.GeneroService;
 
@@ -45,6 +47,13 @@ public class GeneroController {
 	@DeleteMapping(value = "/genero/{id}")
 	public ResponseEntity<?> deletar(@PathVariable("id") Integer id) {
 		this.generoService.delete(id);
+		
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	@PutMapping(value = "/genero")
+	public ResponseEntity<?> editar(@RequestBody @Valid GeneroDTO generoDTO) {
+		this.generoService.update(new Genero(generoDTO.getNome()));
 		
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
